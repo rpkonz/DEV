5cd143438cff990d3906dfa1b8c0e863ea653330
Ryan Konz <rpkon>
2013-12-14 19:28:01 -0500

    Boof js Data representation.

--- greed/index.php
+++ greed/index.php
@@ -22,11 +22,18 @@
 				<div class="gameplay" style="width:200px; margin: 20px auto; cursor:pointer;">
 					<h3>Gameplay</h3>
 				</div>
+				
+				<!-- item prices -->		
+				<div class="item_price" style="width:200px; margin: 20px auto; cursor:pointer; display:none;"> 
+					<h3>Item prices</h3>
+				</div>
 			</div>
+
+			
 			<!-- intro text -->
 			<div id="learn" style="display:none;">
 				<p>Welcome to the land of Greed! This is an early prototype game for a mobile app planned for development in 2014!</p>
-				<p>Greed was designed to be a simple, intuitive game concept for mobile. This is my first attempt at a mobile app and I believe it has the potential to do very well.</p>
+				<p>Greed was designed to be a simple, intuitive game concept for mobile. This is my first attempt at a mobile app and it is intended to be an easy-to-play mobile game that combines a turn-based game with both luck and strategy.</p>
 			</div>			
 			
 			<!-- How it works -->
@@ -41,6 +48,36 @@
 				<p>Gold is used to buy items.</p> 
 				<p>Keep rolling to bank even more gold.</p> 
 				<p style="color:red;">BEWARE: Roll a 1 and you'll lose all your gold from this turn. Roll Snake Eyes and lose all of your items...</p>
+			</div>
+			
+			<div id="item_price" style="display:none;">
+				<ul style="float:left; width:35%; font-size:10px;">
+					<h3>Offensive Items</h3>
+					<!--- AD   -->
+					<h4>Attack Damage Items</h4>
+					<li class="ad1">AD Tier 1  -- 100 Gold</li>
+					<li class="ad2">AD Tier 2  -- 200 Gold</li>
+					<li class="ad3">AD Tier 3  -- 300 Gold</li>
+					<br/>
+					<!-- AP  -->
+					<h4>Ability Power Items</h4>
+					<li class="ap1">AP Tier 1  -- 100 Gold</li>
+					<li class="ap2">AP Tier 2  -- 200 Gold</li>
+					<li class="ap3">AP Tier 3  -- 300 Gold</li>
+				</ul>
+				<ul style="float:right; width:40%; font-size:10px;">
+					<h3>Defensive Items</h3>
+					<!-- Armor  -->		
+					<h4>Armor Defense Items</h4>
+					<li class="arm1">Armor Tier 1  -- 100 Gold</li>
+					<li class="arm2">Armor Tier 2  -- 200 Gold</li>																									<li class="arm3">Armor Tier 3  -- 300 Gold</li>
+					<br/>
+					<!-- MR  -->	
+					<h4>Magic Resist Items</h4>
+					<li class="mr1">MR Tier 1  -- 100 Gold</li>
+					<li class="mr2">MR Tier 2  -- 200 Gold</li>																
+					<li class="mr3">MR Tier 3  -- 300 Gold</li>					
+				</ul>
 			</div>
 			
 			<div class="roll" style="cursor:pointer; float:left;">
@@ -66,7 +103,7 @@
 			
 			<div class="items" style="display:none;">
 				Click an item to equip it. Or save up and buy later.
-				<ul style="float:left; width:40%; height:45%; background:lightgreen;">
+				<ul style="float:left; width:40%; background:lightgreen;">
 					<h3>Offensive Items</h3>
 					<!--- AD   -->
 					<h4>Attack Damage Items</h4>
@@ -80,7 +117,7 @@
 					<li class="ap2">AP Tier 2  -- 200 Gold</li>
 					<li class="ap3">AP Tier 3  -- 300 Gold</li>
 				</ul>
-				<ul style="float:right; width:40%; height:45%; background:lightcoral;">
+				<ul style="float:right; width:40%; background:lightcoral;">
 					<h3>Defensive Items</h3>
 					<!-- Armor  -->		
 					<h4>Armor Defense Items</h4>
@@ -105,7 +142,7 @@
 	        $(function() 
 	        {
 	                $('.learn').avgrund({
-	                        height: 200,
+	                        height: 250,
 	                        holderClass: 'custom',
 	                        showClose: true,
 	                        showCloseText: 'close',
@@ -128,6 +165,14 @@
 	                        onBlurContainer: '.container',
 	                        template: $('#gameplay')		                
 	                });
+	                $('.item_price').avgrund({
+	                        height: 400,
+	                        holderClass: 'custom',
+	                        showClose: true,
+	                        showCloseText: 'close',
+	                        onBlurContainer: '.container',
+	                        template: $('#item_price')		                
+	                });	                
 	        });
 			
 	        var gold = 0;
@@ -144,7 +189,8 @@
 			  	gold += dice1 + dice2;
 		  		console.log("d1 " + dice1);
 		  		console.log("d2 " + dice2);
-		  		
+		  		$('.gameplay').html("<h3>Can I haz help plzz!?!</h3>");
+		  		$('.item_price').show();
 		  		$('.shop').show();
 		  		
 		  		if(dice1 + dice2 == 2)
